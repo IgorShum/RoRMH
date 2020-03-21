10709288d19def23af168edddff5ce72930b9bd0
Igor_Shum <Shumeikoigor@yandex.ru>
2020-03-21 23:01:41 +0300

    +Add Random data to render JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
 	def hello
 		#render :template => "app_controller#hello", :formats => :text
 		#render html: 'ApplicationController' 
-		render json: [123, [909, 'asd', 'bcd', 'ruby', 'rails'], ['count', ['one'], ['two']]]
+
+		render json: ['Random data:', [jsn_par_nm, [jsn_par_nm, jsn_par_nm,jsn_par_nm, jsn_par_nm, jsn_par_nm], [jsn_par_nm, ["#{jsn_par_nm}"], ['two']]]]
+	end
+
+	def jsn_par_nm
+		alp = ('a'..'z').to_a.shuffle[0..10].join
+		dig = (0..9).to_a.shuffle[0..10].join
+		res = alp+dig
+		res2 = res.split('').to_a.shuffle[0..20].join
+		return res2
 	end
 end
