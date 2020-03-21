e113e9a9fad5a85568c59450d83231c40eda7550
Igor_Shum <Shumeikoigor@yandex.ru>
2020-03-21 21:04:57 +0300

    +Add method 'hello' in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+	def hello
+		#render :template => "app_controller#hello", :formats => :text
+		render html: 'ApplicationController' 
+		render html: 'hello'
+	end
 end
