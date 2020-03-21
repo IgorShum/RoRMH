bbc9be38dc7643b7797d8d7b0617a986c7a83c0b
Igor_Shum <Shumeikoigor@yandex.ru>
2020-03-21 21:05:45 +0300

    +Add 'render html\json'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
 		#render :template => "app_controller#hello", :formats => :text
 		render html: 'ApplicationController' 
 		render html: 'hello'
+		render json: ['a',['b'], ['c']]
 	end
 end
